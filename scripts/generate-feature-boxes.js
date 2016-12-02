46e265c0ec29463d9fa8b43339a929fa16982f8b
Chad A. Woitas <woitas.chad@gmail.com>
2016-12-01 20:09:50 -0600

    CSS Complete
    Onto store page js, checkout session storage and xml http request;

--- scripts/generate-feature-boxes.js
+++ scripts/generate-feature-boxes.js
@@ -2,13 +2,13 @@
 for (i=0;i<featureBoxes.length;i++){
 document.writeln("<div class=featureBox id=\""+featureBoxes[i].model+"\"");
 document.writeln("<section class=model>");
-document.writeln("<img src="+featureBoxes[i].photoLocation + ">");
+document.writeln("<div class=\"imgbox\"><img src="+featureBoxes[i].photoLocation + "></div>");
 document.writeln("<p>");
 document.writeln(featureBoxes[i].model);
 document.writeln("</p><p id=\"price\">");
 document.writeln(featureBoxes[i].price);
-document.writeln("<br>");
+document.writeln("</p><br>");
 document.writeln("<button type=\"button\" onclick=\"addToCart()\">Add To Cart </button>");
-document.writeln("</p></section>");
+document.writeln("</section>");
 document.writeln("</div>");
 }
