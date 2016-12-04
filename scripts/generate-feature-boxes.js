51f6fd26f03dd5c1c375385458e7c546d80b3f86
Chad A. Woitas <woitas.chad@gmail.com>
2016-12-04 17:57:56 -0600

    use lab mockbin
    appendchildnode

--- scripts/generate-feature-boxes.js
+++ scripts/generate-feature-boxes.js
@@ -7,29 +7,35 @@
 		 //do things here
 		  var result = JSON.parse(this.responseText);
 		  alert("hi");
-		  var popular = result["Popular Items"];
+		  var allitems = result["Popular Items"];
 		  var strawberry = result["Strawberry Pi"];
 		  var access = result["Strawberry Pi Acessories"];
 		  var soc = result["Industrial Compute Model"];
-		  document.getElementById("newxml").innerHTML = 
-			allitems[i].itemDescription + 
-			allitems[i].itemPrice;
+		  
+		  display(allitems);
 		}
 	};
 	//"http://mockbin.org/bin/a3b756a8-b006-415b-812b-108f98d8ee08/"
-	xmlhttp.open("GET","http://mockbin.org/bin/a3b756a8-b006-415b-812b-108f98d8ee08/",true);
+	var url="https://mockbin.org/bin/bc8ed2d5-1d54-42ca-a910-cfe9ded06d26";
+	
+	xmlhttp.open("GET",url,true);
 	xmlhttp.send();
 	
-/* for (i=0;i<popular.length;i++){
-document.writeln("<div class=featureBox id=\""+popular[i].model+"\"");
-document.writeln("<section class=\"model\">");
-document.writeln("<div class=\"imgbox\"><img src="+popular[i].photoLocation + "></div>");
-document.writeln("<p>");
-document.writeln(popular[i].model);
-document.writeln("</p><p id=\"price\">");
-document.writeln(popular[i].price);
-document.writeln("</p><br>");
-document.writeln("<button type=\"button\" onclick=\"addToCart()\">Add To Cart </button>");
-document.writeln("</section>");
-document.writeln("</div>");
-} */
+function display(objlist){
+for (i=0;i<objlist.length;i++){
+document.getElementById("indexholding").innerHTML = writeln(
+	 "<div class=featureBox id=\""+objlist[i].model+"\""
+    +"<section class=\"model\">"
+    +"<div class=\"imgbox\">"
+	+"<img src="+objlist[i].photoLocation+">"
+	+"</div>"
+    +"<p>" +objlist[i].model +"</p>"
+	+"<p id=\"price\">" +objlist[i].price +"</p>"
+	+"<br>"
+    +"<button type=\"button\" onclick=\"addToCart()\">Add To Cart </button>"
+	+"</section>"
+	+"</div>");
+}
+}
+
+
