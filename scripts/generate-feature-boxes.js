6b6e5dc75c8a5ee327e296ace4054fae0f6fe8e1
Chad A. Woitas <woitas.chad@gmail.com>
2016-12-05 00:17:23 -0600

    Tadah
    Completed personal xmlrequest files. May need to see if possible to
    reduce display function calls. Saving for a safe point of fall back.
    Commiting as final project except for menu js, addtocart functions and
    checkout page. Menu Cart will be completed sunday night, latter two
    monday

--- scripts/generate-feature-boxes.js
+++ scripts/generate-feature-boxes.js
@@ -4,38 +4,42 @@
 	
 	xmlhttp.onreadystatechange = function(){
 		if (this.readyState==4 && this.status==200){
-		 //do things here
 		  var result = JSON.parse(this.responseText);
-		  alert("hi");
-		  var allitems = result["Popular Items"];
+		  var popular = result["Popular Items"];
 		  var strawberry = result["Strawberry Pi"];
 		  var access = result["Strawberry Pi Acessories"];
 		  var soc = result["Industrial Compute Model"];
-		  
-		  display(allitems);
+		
+		//store Page
+		display(strawberry,"StrawberryPi");
+		display(access,"StrawberryPiAcc");
+		display(soc,"soc");
+		
+		//index Page;
+		display(popular,"indexholding");
 		}
 	};
-	//"http://mockbin.org/bin/a3b756a8-b006-415b-812b-108f98d8ee08/"
-	var url="https://mockbin.org/bin/bc8ed2d5-1d54-42ca-a910-cfe9ded06d26";
-	
-	xmlhttp.open("GET",url,true);
+	var urlMyEdit = "http://mockbin.org/bin/cf50b0da-56f2-44cb-9d5d-f898a2cb0c2c";
+	xmlhttp.open("GET",urlMyEdit,true);
 	xmlhttp.send();
 	
-function display(objlist){
-for (i=0;i<objlist.length;i++){
-document.getElementById("indexholding").innerHTML = writeln(
-	 "<div class=featureBox id=\""+objlist[i].model+"\""
+
+function display(objlist,holdingCell){
+for (let i=0;i<objlist.length;i++){
+document.getElementById(holdingCell).innerHTML = 
+	document.getElementById(holdingCell).innerHTML +
+	 "<div class=featureBox id=\""+objlist[i].itemDescription+"\""
     +"<section class=\"model\">"
     +"<div class=\"imgbox\">"
-	+"<img src="+objlist[i].photoLocation+">"
+	+"<img src="+objlist[i].itemImageSrc+">"
 	+"</div>"
-    +"<p>" +objlist[i].model +"</p>"
-	+"<p id=\"price\">" +objlist[i].price +"</p>"
+    +"<p>" +objlist[i].itemDescription +"</p>"
+	+"<p id=\"price\">" +objlist[i].itemPrice +"</p>"
 	+"<br>"
     +"<button type=\"button\" onclick=\"addToCart()\">Add To Cart </button>"
 	+"</section>"
-	+"</div>");
+	+"</div>";
 }
-}
+};
 
 
